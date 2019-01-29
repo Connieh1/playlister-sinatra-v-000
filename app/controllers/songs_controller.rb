f494419b79e9a353e85c61d80bb1bc8408eaa284
Connie Hyman <constance.hyman@mchsi.com>
2019-01-29 01:08:17 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,14 +12,9 @@ class SongsController < ApplicationController
     erb :'/songs/new'
   end
 
-  get '/songs/:slug' do
-    @song = Song.find_by_slug(params[:slug])
-    erb :'songs/show'
-  end
-
   post '/songs' do
-    @song = Song.create(:name => params["Name"])
-    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+    @song = Song.create(name: params["Name"])
+    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
     @song.genre_ids = params[:genres]
     @song.save
 
@@ -27,6 +22,12 @@ class SongsController < ApplicationController
 
     redirect("/songs/#{@song.slug}")
   end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/show'
+  end
+
 
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
