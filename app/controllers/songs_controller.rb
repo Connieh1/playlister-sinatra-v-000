079db666fb41656f5281682acd991105110ea696
Connie Hyman <constance.hyman@mchsi.com>
2019-01-29 00:40:16 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -33,16 +33,16 @@ class SongsController < ApplicationController
     erb :'songs/edit'
   end
 
- patch '/songs/:slug' do
-   @song = Song.find_by_slug(params[:slug])
-   @song.update(params[:song])
-   @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
-   @song.genre.id = Genre.find_or_create_by(params[:genres][])
-   @song.save
+  patch '/songs/:slug' do
+     @song = Song.find_by_slug(params[:slug])
+     @song.update(params[:song])
+     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+     @song.genre_ids = params[:genres]
+     @song.save
 
-  #  flash[:message] = "Song successfully updated."
-   redirect '/songs/#{@songs.params.slug}'
- end
+     flash[:message] = "Successfully updated song."
+     redirect("/songs/#{@song.slug}")
+   end
 
 
 end
